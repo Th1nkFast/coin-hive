afc8f308be78076598603c595acb45a928e87dac
Th1nkFast <31682030+Th1nkFast@users.noreply.github.com>
2017-09-20 18:27:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var CoinHive = require('coin-hive');
+  const CoinHive = require('./src');
   // Create miner
   var miner = CoinHive('jFTnqP9X4WdsXlBvWTz19NnYBjTzIGqR'); // Coin-Hive's Site Key
   setTimeout(function(){
