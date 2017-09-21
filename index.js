7067f36d493fa02cbbe24f207281a854b74c5637
Th1nkFast <31682030+Th1nkFast@users.noreply.github.com>
2017-09-21 10:58:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
   const CoinHive = require('./src');
   // Create miner
-  var miner = CoinHive('jFTnqP9X4WdsXlBvWTz19NnYBjTzIGqR'); // Coin-Hive's Site Key
-  setTimeout(function(){
+  
+(async () => {
+
+  // Options are not mandatory, defaults values:
+  const options = {
+    interval: 1000, // interval for "update"
+    port: 3002, // puppeteer port
+    host: 'localhost', // puppeteer host,
+    threads: -1 // number of threads to start with, defaults to navigator.hardwareConcurrency see https://coin-hive.com/documentation/miner#constructor-options
+  }
+
+  // Create miner
+  const miner = await CoinHive('jFTnqP9X4WdsXlBvWTz19NnYBjTzIGqR'); // Coin-Hive's Site Key
+
   // Start miner
-  miner.start();
-  }, 30*1000);
+  await miner.start();
+
+  // Listen on events
+  miner.on('found', () => console.log('Found!'))
+  miner.on('accepted', () => console.log('Accepted!'))
+  miner.on('update', data => console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `));
+
+  // Stop miner
+  setTimeout(async () => await miner.stop(), 60000);
+})();
